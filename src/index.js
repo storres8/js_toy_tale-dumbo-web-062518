08c959f7804928320d746f2878484308a57fa64a
storres8 <stevetorres7562@gmail.com>
2018-08-18 21:59:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
@@ -10,10 +13,85 @@ addBtn.addEventListener('click', () => {
   if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
+    const toyName = document.getElementById('new-toy-name')
+    const toyImg = document.getElementById('new-toy-img')
+    const submitBtn = document.getElementById('new-toy-submit')
+    // submit
+    submitBtn.addEventListener('click', () =>{
+      const toyObj = {
+        name: toyName.value,
+        image: toyImg.value,
+        likes: 0
+      }
+      createToy(toyObj)
+    })
   } else {
     toyForm.style.display = 'none'
   }
 })
 
+////////////////////////////Started my code from here//////////////////////////////////////////////////////////
 
-// OR HERE!
+fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(resp => allToys(resp))
+
+
+function createToy(toyObj){
+  fetch('http://localhost:3000/toys', {
+  method: 'POST',
+  body: JSON.stringify(toyObj),
+  headers:{
+    'Content-Type': 'application/json'
+  }
+    })
+  .then(res => res.json())
+}
+
+function updateLikes(id, likeValue){
+// console.log("this is the toyid", id)
+  fetch(`http://localhost:3000/toys/${id}`, {
+  method: 'PATCH',
+  body: JSON.stringify(likeValue),
+  headers:{
+    'Content-Type': 'application/json'
+  }
+})
+.then(res => res.json())
+}
+
+
+function allToys(toysArray){
+  const collection = document.getElementById('toy-collection')
+  toysArray.forEach(toy => {
+    const card = document.createElement('div')
+    card.className = "card"
+    const name = document.createElement('h2')
+    name.innerText = toy.name
+    const img = document.createElement('img')
+    img.src = toy.image
+    img.className = "toy-avatar"
+    const likes = document.createElement('p')
+    likes.innerText = `${toy.likes} Likes!`
+    const bttn = document.createElement('button')
+    bttn.className = "like-btn"
+    bttn.innerText = "Like <3"
+    bttn.addEventListener('click', ()=>{
+      const likeValue = {likes: ++toy.likes}
+      updateLikes(toy.id, likeValue)
+      likes.innerText = `${toy.likes} Likes!`
+      // console.log(toy.likes)
+    })
+    card.append(name, img, likes, bttn)
+    collection.append(card)
+
+
+  })
+}
+
+
+
+
+
+
+})
